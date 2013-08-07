84cf1c31fb4c272f76d21dcafec32511f625b109
Jennifer Song <jehsong@gmail.com>
2013-08-06 23:54:05 -0700

    Making problems look better with ord(), csv and higher order problems

--- solutions/problem22.py
+++ solutions/problem22.py
@@ -1,19 +1,25 @@
-import re
+import csv
 
-AlphabeticalValues = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
+names = {}
 
-def names_value_sum(filename):
-	with open(filename) as names:
-		sorted_list = sorted(re.split(',', re.sub('\"', '', names.read())))
+def calculate_word_value(word):
+	return names[word] * sum(map(calculate_letter_value, word))
 	
-		nth = 1
-		total = 0
-		for name in sorted_list:
-			sum = 0
-			for letter in name:
-				sum += AlphabeticalValues[letter]
-				total += sum * nth
-				nth += 1
-	return total
+def calculate_letter_value(letter):
+	return ord(letter) - 64
+
+def create_names(filename):
+	with open(filename, "r") as name_file:
+		reader = csv.reader(name_file, delimiter=",")
+		x = 1
+		for row in reader:
+			row.sort()
+			for name in row:
+				names[name] = x
+				x += 1
+			
+def names_value_sum(filename):
+	create_names(filename)
+	return sum(map(calculate_word_value, names))
 	
 print names_value_sum("../files/names.txt")
--- solutions/problem42.py
+++ solutions/problem42.py
@@ -4,13 +4,11 @@ words = []
 word_values = []
 triangle_numbers = []
 
-AlphabeticalValues = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
-
 def calculate_word_value(word):
 	return sum(map(calculate_letter_value, word))
 	
 def calculate_letter_value(letter):
-	return AlphabeticalValues[letter]
+	return ord(letter) - 64
 
 def create_words(filename):
 	with open(filename, "r") as word_file:
